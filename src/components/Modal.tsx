9b5a7cddab6f37295020c9390efe748b398d1e75
Syed Ansur Mehdi <ansurmehdi@gmail.com>
2024-10-08 16:23:49 +0200

    modal styling, add close icon

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { Gif } from "../types";
 import styled from "styled-components";
+import { CloseIcon } from "../assets/icons";
 
 interface ModalProps {
   gif: Gif;
@@ -17,9 +18,10 @@ const StyledModalContainer = styled.div`
   overflow: auto;
   background-color: rgba(0, 0, 0, 0.5);
   z-index: 1;
-  transition: transform 0.3s ease-in-out;
   &.open {
-    display: block;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 `;
 
@@ -27,66 +29,71 @@ const StyledModalContent = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
   position: relative;
   width: 80%;
-  height: 80%;
   background-color: #fefefe;
   margin: auto;
   padding: 20px;
   border: 1px solid #888;
-  // top: 10%;
-  // transform: translateY(-10%);
   border-radius: 1rem;
+  gap: 0.5rem;
 `;
 
-const StyledCloseButton = styled.button`
-  color: #aaa;
-  float: right;
-  font-size: 28px;
-  font-weight: bold;
+const StyledHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
+
+const StyledTitle = styled.h2`
+  font-size: 1rem;
+  font-weight: 700;
+  max-width: 80%;
+`;
+
+const StyledCloseButton = styled.span`
   cursor: pointer;
   &:hover,
   &:focus {
     color: black;
     text-decoration: none;
-    cursor: pointer;
   }
 `;
 
-const StyledTitle = styled.h2`
-  font-size: 2rem;
-  font-weight: 700;
-  margin-top: 1rem;
-  text-align: center;
-`;
-
 const StyledDivider = styled.div`
-  width: 90%;
+  width: 100%;
   height: 1px;
-  background-color: #000;
-  margin: 1rem 0;
+  background-color: #e0e0e0;
+  margin: 0.5rem 0;
 `;
 
 const StyledGif = styled.img`
   width: 100%;
-  height: 100%;
   object-fit: contain;
   aspect-ratio: 1/1;
   max-width: 480px;
   max-height: 480px;
   border-radius: 1rem;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  margin: 1rem 0;
 `;
 
 export const Modal: FC<ModalProps> = ({ gif, onClose }) => {
   return (
     <StyledModalContainer className="open">
       <StyledModalContent>
-        <StyledCloseButton onClick={onClose}>Close</StyledCloseButton>
+        <StyledHeader>
+          <StyledTitle>
+            {gif.title.substring(0, 100)}
+            {gif.title.length > 100 ? "..." : ""}
+          </StyledTitle>
+          <StyledCloseButton onClick={onClose}>
+            <CloseIcon />
+          </StyledCloseButton>
+        </StyledHeader>
         <StyledDivider />
         <StyledGif src={gif.images.original.url} alt={gif.alt_text} />
-        <StyledTitle>{gif.title}</StyledTitle>
       </StyledModalContent>
     </StyledModalContainer>
   );
