09a1977133fe98d33f0014226eba5399f1d39789
Syed Ansur Mehdi <ansurmehdi@gmail.com>
2024-10-08 02:17:10 +0200

    add api and env variables

--- src/api.ts
+++ src/api.ts
@@ -9,3 +9,41 @@ interface GifResponse {
     response_id: string;
   };
 }
+
+interface GetTrendingGifsParams {
+  offset: number;
+  limit: number;
+}
+
+export const getTrendingGifs = async ({
+  offset,
+  limit,
+}: GetTrendingGifsParams): Promise<GifResponse> => {
+  const response = await fetch(
+    `${import.meta.env.VITE_GIPHY_API_URL}/trending?api_key=${
+      import.meta.env.VITE_GIPHY_API_KEY
+    }&limit=${limit}&offset=${offset}`
+  );
+  const data = await response.json();
+  return data;
+};
+
+interface GetSearchGifsParams {
+  query: string;
+  offset: number;
+  limit: number;
+}
+
+export const getSearchGifs = async ({
+  query,
+  offset,
+  limit,
+}: GetSearchGifsParams): Promise<GifResponse> => {
+  const response = await fetch(
+    `${import.meta.env.VITE_GIPHY_API_URL}/search?api_key=${
+      import.meta.env.VITE_GIPHY_API_KEY
+    }&q=${query}&limit=${limit}&offset=${offset}`
+  );
+  const data = await response.json();
+  return data;
+};
